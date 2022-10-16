b665c23f9647876f9d61491af20ae6626c977c5e
Miguel Raveli <miguelraveli123@gmail.com>
2022-10-16 01:56:12 -0300

    first commit

---
+++ frontend/src/components/MainCardProduct.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import { FaCartArrowDown } from 'react-icons/fa';
+
+import MainRating from './MainRating';
+
+import './MainCardProduct.css';
+
+
+export default function MainCardProduct(props) {
+
+    const { product, showLink = false } = props;
+
+    function handleIconCardProductClick (idOfProductSelected) {
+        
+        // aciona o modal
+        const objDivCardProduct = document.querySelector(`.cardId-${idOfProductSelected}`);
+        const objDivBlurContainer = document.querySelector(`.containerBlur-ByProductId-${idOfProductSelected}`);
+
+        objDivCardProduct.classList.toggle("isActive");
+        objDivBlurContainer.classList.toggle("isActive");
+
+
+        const objIcon =  document.querySelector(`.iconId-${idOfProductSelected}`); 
+
+        // pega o produto que foi adicionado no cart
+
+
+        // finalmente adiciona no cart
+        // props.history.push(`/cart/${product.id}`);
+    }
+
+    function handleCancelModalSingleProductClick(idOfProductSelected) {
+        const objDivCardProduct = document.querySelector(`.cardId-${idOfProductSelected}`);
+        const objDivBlurContainer = document.querySelector(`.containerBlur-ByProductId-${idOfProductSelected}`);
+
+        objDivCardProduct.classList.toggle("isActive");
+        objDivBlurContainer.classList.toggle("isActive");
+    }
+
+    function handleConfirmModalSingleProductClick(idOfProductSelected) {
+        // desativa modal
+        const objDivCardProduct = document.querySelector(`.cardId-${idOfProductSelected}`);
+        const objDivBlurContainer = document.querySelector(`.containerBlur-ByProductId-${idOfProductSelected}`);
+
+        objDivCardProduct.classList.toggle("isActive");
+        objDivBlurContainer.classList.toggle("isActive");
+
+        // seta no icon do singleCardProduct a quantidade de produto selecionado
+        const objInputQtdOfProduct = document.querySelector(`#productSingleQtdOfId-${idOfProductSelected}`)
+        var quantidadeDesejada = Number(objInputQtdOfProduct.value);
+
+
+        const objIconSingleProduct = document.querySelector(`.iconId-${idOfProductSelected}`);
+        const filhoQtdProdutos = document.createElement("span");
+        filhoQtdProdutos.setAttribute("id", `qtdSelecionadaByProductId-${idOfProductSelected}`)
+
+        // verifica se é outro contexto e evita re-append
+        if(objIconSingleProduct.children[0].childElementCount > 1){
+            objIconSingleProduct.children[0].children[1].textContent = quantidadeDesejada;
+        }else {
+            filhoQtdProdutos.appendChild(document.createTextNode(quantidadeDesejada));
+            objIconSingleProduct.children[0].appendChild(filhoQtdProdutos);
+        }
+       
+    }
+
+    return (
+    <>
+    <div key={product.id} className="main-cardProduct">
+        <Link to={ showLink ? `/product/${product.id}`: '#'}>
+            <div className="wrapperImage">
+                <img className="img--mediumSize" src={product.image} alt={product.description} />
+                <span className={`iconId-${product.id}`} onClick={() => handleIconCardProductClick(product.id)}><i><FaCartArrowDown /></i></span>
+                <div className="burger-title--container">
+                    <h1 className="burger-title">{product.name}</h1>
+                    <span className="burger-price"><p>{Intl.NumberFormat("en-US", {
+                                                style: "currency", 
+                                                currency: "USD"}).format(Number(product.price))}</p></span>
+                </div>
+            </div>              
+            <MainRating id={product.id} rating={product.rating} numReviews={product.numReviews} />
+        </Link>
+    </div>
+    <div className={`containerBlur containerBlur-ByProductId-${product.id}`} >
+
+    </div>    
+    <div className={`modalSingleProductClick cardId-${product.id}`}>
+        <div>
+            <h1 className="burger-title colorGreen">{product.name} <MainRating id={product.id} rating={product.rating} /></h1>
+            <p className="descriptionOfSingleProduct">
+                {product.description}
+            </p>
+            <div className="containerQuantidadeProduto">
+                <label>Quantidade desejada?</label>
+                <input id={`productSingleQtdOfId-${product.id}`} type="number" min="1" max="100"/>
+            </div> 
+            <div className="containerButtonsModalSingleProduct">
+                <button onClick={() => handleConfirmModalSingleProductClick(product.id)} className="btnModalSingleProduct">Confirmar</button>
+                <button onClick={() => handleCancelModalSingleProductClick(product.id)} className="btnModalSingleProduct -danger">Cancelar</button>
+            </div>
+        </div>
+    </div>
+    </>
+    )
+};
---
+++ frontend/src/data.js
@@ -0,0 +1,90 @@
+const data = {
+    products: [
+        {
+            id:'1',
+            name:'X-Grelhadinho',
+            category: 'Shirts',
+            image: '/images/grelhadinhu.jpeg',
+            price: 120.5,
+            stock: 10,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 1058,
+            description: 'X-Grelhadinho: Alface, 2 hambúrgueres, queijo e picles'
+        },
+        {
+            id:'2',
+            name:'X-Burgue',
+            category: 'Shirts',
+            image: '/images/anabolizadu.png',
+            price: 1000.25,
+            stock: 12,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 11,
+            description: 'Injetadinhun dlç “Isso não pode estar acontecendo. Toda vez que fico mais forte, um poder maior parece me desafiar. É como se o destino me zombasse com um sorriso, assim como Kakaroto.” “Você não é derrotado quando perde, mais sim quando você desiste” “Não existe justiça ou injustiça em uma batalha. Só há vitória ou, no seu caso, derrota” “Você pode ter invadido minha mente e meu corpo, mas há uma coisa que um Saiyajin sempre mantém. SEU ORGULHO!”'
+        },
+        {
+            id:'3',
+            name:'X-Tomate',
+            category: 'Shirts',
+            image: '/images/heroinadao.jpg',
+            price: 1001.25,
+            stock: 17,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'Breaking bad patrocina “Isso não pode estar acontecendo. Toda vez que fico mais forte, um poder maior parece me desafiar. É como se o destino me zombasse com um sorriso, assim como Kakaroto.” “Você não é derrotado quando perde, mais sim quando você desiste” “Não existe justiça ou injustiça em uma batalha. Só há vitória ou, no seu caso, derrota” “Você pode ter invadido minha mente e meu corpo, mas há uma coisa que um Saiyajin sempre mantém. SEU ORGULHO!”'
+        },
+        {
+            id:'4',
+            name:'X-Cebola',
+            category: 'Shirts',
+            image: '/images/mordido.jpg',
+            price: 2500.25,
+            stock: 25,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10255,
+            description: 'Esse vai mordido mesmo kakakakak “Isso não pode estar acontecendo. Toda vez que fico mais forte, um poder maior parece me desafiar. É como se o destino me zombasse com um sorriso, assim como Kakaroto.” “Você não é derrotado quando perde, mais sim quando você desiste” “Não existe justiça ou injustiça em uma batalha. Só há vitória ou, no seu caso, derrota” “Você pode ter invadido minha mente e meu corpo, mas há uma coisa que um Saiyajin sempre mantém. SEU ORGULHO!”'
+        },
+        {
+            id:'5',
+            name:'X-Galinha',
+            category: 'Shirts',
+            image: '/images/linguinha.jpg',
+            price: 20,
+            stock: 0,
+            brand: 'Nike Made in China',
+            rating: 5,
+            numReviews: 10,
+            description: 'Sem criatividade pra issu “Isso não pode estar acontecendo. Toda vez que fico mais forte, um poder maior parece me desafiar. É como se o destino me zombasse com um sorriso, assim como Kakaroto.” “Você não é derrotado quando perde, mais sim quando você desiste” “Não existe justiça ou injustiça em uma batalha. Só há vitória ou, no seu caso, derrota” “Você pode ter invadido minha mente e meu corpo, mas há uma coisa que um Saiyajin sempre mantém. SEU ORGULHO!”'
+        },
+        {
+            id:'6',
+            name:'X-Simples',
+            category: 'Shirts',
+            image: '/images/entupiuMinhaCarotida.png',
+            price: 22,
+            stock: 145,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 13,
+            description: 'Utima dose “Isso não pode estar acontecendo. Toda vez que fico mais forte, um poder maior parece me desafiar. É como se o destino me zombasse com um sorriso, assim como Kakaroto.” “Você não é derrotado quando perde, mais sim quando você desiste” “Não existe justiça ou injustiça em uma batalha. Só há vitória ou, no seu caso, derrota” “Você pode ter invadido minha mente e meu corpo, mas há uma coisa que um Saiyajin sempre mantém. SEU ORGULHO!”'
+        },
+        {
+            id:'7',
+            name:'X-Grandão',
+            category: 'pipipipopopo',
+            image: '/images/vegetta.jpg',
+            price: 22,
+            stock: 145,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 133,
+            description: 'Principe dos sayajin “Isso não pode estar acontecendo. Toda vez que fico mais forte, um poder maior parece me desafiar. É como se o destino me zombasse com um sorriso, assim como Kakaroto.” “Você não é derrotado quando perde, mais sim quando você desiste” “Não existe justiça ou injustiça em uma batalha. Só há vitória ou, no seu caso, derrota” “Você pode ter invadido minha mente e meu corpo, mas há uma coisa que um Saiyajin sempre mantém. SEU ORGULHO!”'
+        },
+    ]
+}
+
+export default data;
